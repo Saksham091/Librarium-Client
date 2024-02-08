bf3e1605b4e1a13d3511c239ba78f2bef63b2ccf
Saksham091 <saksham.agarwal@kalvium.community>
2024-02-08 18:02:18 +0530

    Test issue resolved

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import Loader from './Components/Loader/Loader'
 import Login from './Components/Login/Login'
-import Test from './Components/Test/Test'
 import Book from './Components/Book/Book'
 import Wishlist from './Components/Wishlist/Wishlist'
 import Error from './Components/Error/Error'
@@ -17,7 +16,6 @@ function App() {
         <Route path='/loader' element={<Loader />} />
         <Route path='/' element={<Login />} />
         <Route path='/home' element={<Home />} />
-        <Route path='/test' element={<Test />} />
         <Route path='/wishlist' element={<Wishlist />} />
         <Route path="/book/:id" element={<Book />} />
         <Route path='/cart' element={<Cart />} />
